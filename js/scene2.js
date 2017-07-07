dd32992228f22c38577439325cf4041b23dcbbd0
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-07-07 15:27:30 -0400

    scene3 stage set up

---
+++ js/scene2.js
@@ -0,0 +1,39 @@
+function scene2() {
+
+
+  stage = document.querySelector('#stage')
+
+
+  // Setup Cast
+
+  // Character 1
+
+  // Character 2
+
+  // Setup Props
+
+  // Prop 1
+
+  // Prop 2
+
+  // Action!
+
+  // Stage direction 1
+
+  frame(function() {
+
+  })
+
+  // Stage direction 2
+
+  frame(function() {
+
+  })
+
+}
+
+function scene2() {
+  console.log('Executing scene 2!')
+
+  // ...
+}
