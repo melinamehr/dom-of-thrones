a8ab4fc29831466770107f63311fe324d0d4e792
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-07-10 13:05:01 -0400

    setting up characters in scene2

--- js/scene2.js
+++ js/scene2.js
@@ -1,10 +1,25 @@
+console.log('Executing scene 2')
+
 function scene2() {
 
 
+ // Setup Stage
   stage = document.querySelector('#stage')
-
+  castleImgSrc = castle.getAttribute('src')
+  stageImg.setAttribute('src', castleImgSrc)
 
   // Setup Cast
+  nedContainer = document.createElement('div')
+  stage.append(nedContainer)
+  nedContainer.style.position = 'absolute'
+  nedContainer.style.width = '10%'
+  nedContainer.style.height = '20%'
+  nedContainer.style.top = '50%'
+  nedContainer.style.left = '50%'
+  ned = document.querySelector('#ned')
+  nedContainer.append(ned)
+  leather = document.querySelector('#leather-armour')
+  nedContainer.append(leather)
 
   // Character 1
 
